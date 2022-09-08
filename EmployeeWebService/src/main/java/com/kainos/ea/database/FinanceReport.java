120e7b32f5b92d7c18d0450bd34ea35c3e309df3
KarimL <karim.latchman@kainos.com>
2022-09-08 15:12:14 +0100

    User story 4 completed assuming Normal employees

---
+++ EmployeeWebService/src/main/java/com/kainos/ea/database/FinanceReport.java
@@ -0,0 +1,53 @@
+package com.kainos.ea.database;
+
+import com.kainos.ea.employee_stuff.Employee;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.OptionalDouble;
+
+import static com.kainos.ea.database.Database.getConnection;
+
+public class FinanceReport {
+
+    private static Connection myConnection;
+    public static String department;
+    public static List<String> getFinanceReport() {
+
+        List<Employee> bigEmps = new ArrayList<>();
+        List<String> financeR = new ArrayList<>();
+        ResultSet rs = null;
+
+        try (Connection myConnection = getConnection();
+             Statement st = (myConnection == null) ? null : myConnection.createStatement()) {
+
+            if (myConnection == null)
+                throw new SQLException("Database connection null");
+
+            rs = st.executeQuery("select * from employee_JackH");
+
+
+            while (rs.next()) {
+                Employee dbEmp = new Employee((short) rs.getInt("EmployeeNo"),
+                        rs.getString("EmployeeName"), rs.getString("Address"),
+                        rs.getString("NIN"),rs.getString("BankAccountNo"),
+                        rs.getInt("Salary"), rs.getString("Department"));
+                financeR.add(String.format(dbEmp.getNumber() + ": " + dbEmp.getName() + ", £%,.2f." , (float)dbEmp.calcPay()));
+                bigEmps.add(dbEmp);
+            }
+
+
+        } catch (SQLException ex) {
+
+            System.out.println(ex.getMessage());
+
+        }
+
+        return financeR;
+
+    }
+}
