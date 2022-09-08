c5c7aaf963196b297ed267874f730671e7956ecf
KarimL <karim.latchman@kainos.com>
2022-09-08 10:23:37 +0100

    Fresh Project

---
+++ EmployeeWebService/src/main/java/com/kainos/ea/resources/WebService.java
@@ -0,0 +1,17 @@
+package com.kainos.ea.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/api")
+public class WebService {
+    @GET
+    @Path("/print/{msg}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getMsg(@PathParam("msg") String message) {
+        return "Hello from a RESTful Web service: " + message;
+    }
+}
