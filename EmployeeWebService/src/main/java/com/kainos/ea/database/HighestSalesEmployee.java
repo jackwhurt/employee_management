d0265baa92aec40f0248a7ca65235e5aacb6203a
Jennifer Choo <jennifer.choo@kainos.com>
2022-09-08 15:31:38 +0100

    Fixing highest sales query

--- EmployeeWebService/src/main/java/com/kainos/ea/database/HighestSalesEmployee.java
+++ EmployeeWebService/src/main/java/com/kainos/ea/database/HighestSalesEmployee.java
@@ -14,20 +14,21 @@ public class HighestSalesEmployee {
 
     public static SalesEmployee getHighestSalesEmployee(){
 
-        ResultSet rs;
-        SalesEmployee dbEmp = new SalesEmployee ();
+        ResultSet rs = null;
+        SalesEmployee dbEmp = null;
         try (Connection myConnection = getConnection();
              Statement st = (myConnection == null) ? null : myConnection.createStatement()) {
 
             if (myConnection == null)
                 throw new SQLException ("Database connection null");
 
-            rs = st.executeQuery("select * from SalesEmployee_JackH where SalesTotal IN (select max(SalesTotal) from SalesEmployee_JackH);");
-
-                        dbEmp.setNumber((short) rs.getInt("EmployeeNo"));
-                        dbEmp.setName (rs.getString("EmployeeName"));
-                        dbEmp.setCommission (rs.getInt ("Commission"));
-                        dbEmp.setSalesTotal (rs.getInt("SalesTotal"));
+            while (rs.next()) {
+                rs = st.executeQuery ("select * from SalesEmployee_JackH where SalesTotal IN (select max(SalesTotal) from SalesEmployee_JackH);");
+                dbEmp = new SalesEmployee ((short) rs.getInt ("EmployeeNo"),
+                        rs.getString ("EmployeeName"),
+                        rs.getInt ("Commission"),
+                        rs.getInt ("SalesTotal"));
+            }
 
         } catch (SQLException ex) {
 
