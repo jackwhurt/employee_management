bb161809bd2631b7772a60aec07eedd532101515
Jack Hurt <jackwhurt@gmail.com>
2022-09-09 14:07:33 +0100

    added file

---
+++ EmployeeWebService/src/main/java/test/com/kainos/ea/database/EnterEmployeesTest.java
@@ -0,0 +1,35 @@
+package test.com.kainos.ea.database; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* EnterEmployees Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Sept 9, 2022</pre> 
+* @version 1.0 
+*/ 
+public class EnterEmployeesTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: enterEmployees(List<Employee> employeesToAdd) 
+* 
+*/ 
+@Test
+public void testEnterEmployees() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
